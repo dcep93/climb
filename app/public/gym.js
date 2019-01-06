c542bed6746c2b106b0ae01ea7d755ec14b1f3cd
dcep93 <dan@quizlet.com>
2019-01-06 13:38:00 -0800

    unnecessary js

--- app/public/gym.js
+++ app/public/gym.js
@@ -4,27 +4,4 @@ $(document).ready(function() {
         var endpoint = location.href+'/'+wall_id+'/climb';
         $.post(endpoint, { climbed: this.checked });
     });
-
-    $('form.wall-update-form').bind('input', function() {
-        $(this).css('border-color', 'red');
-    });
-
-    $('form.wall-update-form').submit(function(e) {
-        var wall_id = $(this).closest('.wall').attr('data-wall-id');
-        var endpoint = location.href+'/'+wall_id+'/edit';
-        $(this).css('border-color', 'yellow');
-        $.post(endpoint, $(this).serialize(), (function() { $(this).css('border-color', ''); }).bind(this));
-        return false;
-    });
-
-    $('form.new-wall-form').submit(function(e) {
-        var endpoint = location.href+'/new';
-        $(this).css('border-color', 'yellow');
-        $.post(endpoint, $(this).serialize(), refresh);
-        return false;
-    });
-
-    var dateParts = new Date().toLocaleDateString('en-US').split('/');
-    var today = dateParts[2]+'-'+dateParts[0]+'-'+dateParts[1];
-    $('form.new-wall-form input[name=date]').val(today);
 });
