85eda95637d7beb49d3c3315b4c9c1ff58d6efc8
dcep93 <dan@quizlet.com>
2019-02-12 13:26:18 -0800

    ts-ignore

--- app/client/src/components/App/Main/GymsAdmin.tsx
+++ app/client/src/components/App/Main/GymsAdmin.tsx
@@ -18,14 +18,18 @@ class GymsAdmin extends Component<object, Readonly<typeof initialState>> {
       event.preventDefault();
     }
 
+    getTextArea = () => {
+      // @ts-ignore Type 'HTMLTextAreaElement' is missing the following properties from type 'HTMLInputElement': accept, align, alt, checked, and 23 more.  TS2322
+      return <textarea {...g.input(this, "descrption")}/>;
+    }
+
     render(): any {
       return (
         <form onSubmit={this.submit}>
             <p>New Gym</p>
             <p>path: <input {...g.input(this, "path", gt.InputType.Text)}/></p>
             <p>name: <input {...g.input(this, "name", gt.InputType.Text)}/></p>
-            // @ts-ignore Type 'HTMLTextAreaElement' is missing the following properties from type 'HTMLInputElement': accept, align, alt, checked, and 23 more.  TS2322
-            <p>description: <textarea {...g.input(this, "descrption")}/></p>
+            <p>description: {this.getTextArea()}</p>
             <input type="submit" />
         </form>
       );
