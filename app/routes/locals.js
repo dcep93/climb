b5156e00482435831dfd6991a2a666a534eb4f80
dcep93 <dan@quizlet.com>
2019-02-17 01:09:32 -0800

    missed

--- app/routes/locals.js
+++ app/routes/locals.js
@@ -9,7 +9,7 @@ function locals(req, res, next) {
     res.data = (json) => res.json(Object.assign({common: res.common}, json));
     var userId = req.session.userId;
     if (userId !== undefined) {
-        orm(null, null, next).select({table: 'users', where: {id: userId}})
+        orm(null, null, next).select('users', {id: userId})
             .then((users) => users[0] || Promise.reject())
             .then((user) => Object.assign(res.common, {user}))
             .then(() => next());
--- app/routes/user.js
+++ app/routes/user.js
@@ -6,7 +6,7 @@ var app = express.Router({mergeParams: true});
 
 app.get("/", function(req, res, next) {
     var userId = req.params.user_id;
-    orm(null, null, next).select({table: 'users', where: {id: userId}})
+    orm(null, null, next).select('users', {id: userId})
         .then((users) => users[0] || Promise.reject())
         .then((user) => res.data({user}));
 });
