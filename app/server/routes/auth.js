b31ff594217243072508401053eea21f38f643ce
dcep93 <dan@quizlet.com>
2019-02-18 17:32:28 -0800

    setAdmin

--- app/server/routes/auth.js
+++ app/server/routes/auth.js
@@ -6,6 +6,10 @@ var config = require("../../config");
 
 var app = express.Router();
 
+function setAdmin(userId, next) {
+    return orm(null, null, next).update('users', {is_admin: true, is_verified: true}, {id: userId});
+}
+
 app.post("/login", function(req, res, next) {
     var idToken = req.body.id_token;
     var client = new OAuth2Client(config.clientId);
@@ -19,7 +23,7 @@ app.post("/login", function(req, res, next) {
             var email = payload["email"];
             var name = payload["name"];
             var image = payload["picture"];
-            orm(null, null, next).insert('users', {
+            return orm(null, null, next).insert('users', {
                     google_id: payload["sub"],
                     email,
                     name,
@@ -28,9 +32,11 @@ app.post("/login", function(req, res, next) {
                     q: 'ON DUPLICATE KEY UPDATE id=LAST_INSERT_ID(id), name=?, image=?',
                     p: [name, image]
                 })
-                    .then((userId) => Object.assign(req.session, {userId}))
-                    .then(() => res.sendStatus(200));
-        }, next);
+            })
+                .then((userId) => Object.assign(req.session, {userId}) && userId)
+                .then((userId) => userId == 1 && setAdmin(userId, next))
+                .then(() => res.sendStatus(200))
+                .catch(next);
 });
 
 app.post("/logout", function(req, res) {
