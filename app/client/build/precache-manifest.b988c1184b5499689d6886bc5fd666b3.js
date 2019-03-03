5e7d297a076d8da2f28deb029124a972d8eea99f
dcep93 <dan@quizlet.com>
2019-03-03 20:15:21 +0900

    adding build

---
+++ app/client/build/precache-manifest.b988c1184b5499689d6886bc5fd666b3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c1a80a600ad80403f346",
+    "url": "/static/js/main.c1a80a60.chunk.js"
+  },
+  {
+    "revision": "019403d13d7a764d15d3",
+    "url": "/static/css/1.df30445b.chunk.css"
+  },
+  {
+    "revision": "019403d13d7a764d15d3",
+    "url": "/static/js/1.019403d1.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d8cb7b38fb4c54855274f2b1ad11f68f",
+    "url": "/index.html"
+  }
+];
