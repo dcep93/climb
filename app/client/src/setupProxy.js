2403c217aeee4bbac03cc54840cc414190e18e39
dcep93 <dan@quizlet.com>
2019-03-04 12:06:47 +0900

    can specify target

--- app/client/src/setupProxy.js
+++ app/client/src/setupProxy.js
@@ -2,10 +2,9 @@ const proxy = require("http-proxy-middleware");
 
 const config = require("../../config.json");
 
-const port = config.port || 8080;
-
-const target = `http://localhost:${port}/`;
+const changeOrigin = Boolean(config.target);
+const target = config.target || `http://localhost:${config.port || 8080}/`;
 
 module.exports = function(app) {
-	app.use(proxy("/api", { target }));
+	app.use(proxy("/api", { target, changeOrigin }));
 };
