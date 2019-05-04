2075845f8a6b11122b712a851d5b5c987256ff51
dcep93 <dan@quizlet.com>
2019-05-04 22:48:12 +0900

    new gym space between

--- app/client/src/components/App/Main/ClimbRouter/Gyms/index.tsx
+++ app/client/src/components/App/Main/ClimbRouter/Gyms/index.tsx
@@ -7,21 +7,28 @@ import g from "../../../../../globals";
 import * as gt from "../../../../../globals";
 import gs from "../../../../../globals.module.css";
 
+import styles from "./index.module.css";
+
 function Gyms(props: { gyms: gt.gymType[] }) {
 	return (
 		<div>
 			<g.Title title="The Climbing App" />
-			<div className={gs.flex}>
-				{props.gyms.map(gym => (
-					<div key={gym.id} className={gs.bubble}>
-						<Link to={`/gym/${gym.path}`}>
-							<h3>{gym.name}</h3>
-							<p>{gym.description}</p>
-						</Link>
-					</div>
-				))}
+			<div className={`${gs.flex} ${styles.gyms_parent}`}>
+				<div className={gs.flex}>
+					{props.gyms.map(gym => (
+						<div
+							key={gym.id}
+							className={`${gs.bubble} ${styles.gym}`}
+						>
+							<Link to={`/gym/${gym.path}`}>
+								<h3>{gym.name}</h3>
+								<p>{gym.description}</p>
+							</Link>
+						</div>
+					))}
+				</div>
+				{g.common().user.is_admin && <GymsAdmin />}
 			</div>
-			{g.common().user.is_admin && <GymsAdmin />}
 		</div>
 	);
 }
