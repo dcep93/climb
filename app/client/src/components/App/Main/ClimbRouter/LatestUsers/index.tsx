bf24638ae421124ca4536318499133c6a3f0ffd2
dcep93 <dan@quizlet.com>
2019-05-05 17:18:41 +0900

    profile

--- app/client/src/components/App/Main/ClimbRouter/LatestUsers/index.tsx
+++ app/client/src/components/App/Main/ClimbRouter/LatestUsers/index.tsx
@@ -5,6 +5,8 @@ import g from "../../../../../globals";
 import * as gt from "../../../../../globals";
 import gs from "../../../../../globals.module.css";
 
+import styles from "./index.module.css";
+
 function LatestUsers(props: { users: gt.userType[] }) {
 	return (
 		<div>
@@ -13,11 +15,15 @@ function LatestUsers(props: { users: gt.userType[] }) {
 				{props.users.map(user => (
 					<div key={user.id} className={gs.bubble}>
 						<Link to={`/user/${user.id}`}>
-							<p>id: {user.id}</p>
-							<p>name: {user.name}</p>
-							<p>email: {user.email}</p>
-							<p>timestamp: {user.timestamp}</p>
-							<img src={user.image} />
+							<p>
+								{user.name} ({user.id})
+							</p>
+							<p>{user.email}</p>
+							<p>{new Date(user.timestamp).toLocaleString()}</p>
+							<img
+								className={styles.profile_pic}
+								src={user.image}
+							/>
 						</Link>
 					</div>
 				))}
