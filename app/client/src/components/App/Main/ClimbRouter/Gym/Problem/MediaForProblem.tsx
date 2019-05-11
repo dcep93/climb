78b7ce1d7a8952c98d363b7284c934704825215d
dcep93 <dan@quizlet.com>
2019-05-11 21:09:07 +0900

    remove unnecessary

--- app/client/src/components/App/Main/ClimbRouter/Gym/Problem/MediaForProblem.tsx
+++ app/client/src/components/App/Main/ClimbRouter/Gym/Problem/MediaForProblem.tsx
@@ -9,49 +9,6 @@ import Media from "./Media";
 import styles from "./Media.module.css";
 import profile_styles from "../../Profile/index.module.css";
 
-function mediaData(m: gt.mediaType) {
-	if (!m.data) {
-		return <p>received - handling</p>;
-	} else if (m.mime === "video") {
-		const href = encodeURIComponent(`https://www.facebook.com${m.data}`);
-		const paddingPercent = (m.height * 100) / m.width;
-		return (
-			<div
-				className={styles.video_div}
-				style={{ paddingTop: `${paddingPercent}%` }}
-			>
-				<iframe
-					className={styles.video}
-					src={`https://www.facebook.com/plugins/video.php?href=${href}`}
-					width={m.width}
-					height={m.height}
-					style={{
-						border: "none"
-					}}
-					scrolling="no"
-					allowFullScreen={true}
-				/>
-				<div className={gs.vertical_space_10} />
-			</div>
-		);
-	} else if (m.mime === "image") {
-		return <img className={styles.image} src={m.data} />;
-	} else {
-		return <pre>{m.data}</pre>;
-	}
-}
-
-function profileLink(
-	m: gt.mediaType,
-	problemsDict: { [id: number]: gt.problemType }
-) {
-	return (
-		<Link to={`/gym/${m.gym_path}/problem/${m.problem_id}`}>
-			<div className={gs.padding}>{problemsDict[m.problem_id].name}</div>
-		</Link>
-	);
-}
-
 function problemLink(
 	m: gt.mediaType,
 	usersDict: { [id: number]: gt.userType }
