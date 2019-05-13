92de98e9652c83b4ea4ef9a2fb3f70b88b76de2b
dcep93 <dan@quizlet.com>
2019-05-13 15:20:30 +0900

    remote reset db

--- app/server/routes/admin_nodemon.js
+++ app/server/routes/admin_nodemon.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const exec = require("child_process").exec;
 
+const config = require("../../config");
+
 const app = express.Router();
 
 app.use("/pull", function(req, res) {
@@ -21,6 +23,21 @@ app.use("/pull", function(req, res) {
 app.use("/rs", function(req, res) {
 	res.sendStatus(200);
 	process.kill(process.pid, "SIGUSR2");
+});
+
+app.use("/reset_db", function(req, res) {
+	exec(
+		`mysqldump -u root -p${config.mysql_password} climb | mysql -u root -p${
+			config.mysql_password
+		} remote`,
+		function(err, stdout, stderr) {
+			if (err) {
+				console.log(stderr);
+				return res.status(500).send(`<pre>${stderr}</pre>`);
+			}
+			res.send("success");
+		}
+	);
 });
 
 module.exports = app;
